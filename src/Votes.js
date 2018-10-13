0a64b464596f416d3693d76f731eef9d7fba2bf0
Connor <elcondordelanoche@gmail.com>
2018-10-13 11:52:30 -0500

    votes

---
+++ src/Votes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Votes.css';
+// import elon from './elonsmoke.png';
+// import elon from './elonsmoke.png';
+
+class Votes extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const listItems = this.props.solutions.map((solution) =>
+        <div>
+          <div>{solution.votes} {solution.text}</div>
+        </div>
+    );
+    return (
+        <div className="votes-grid">
+          <div className="solution-problem">{this.props.problem}</div>
+          <div className="solutions-votes">{listItems}</div>
+        </div>
+    );
+  }
+}
+
+export default Votes;
