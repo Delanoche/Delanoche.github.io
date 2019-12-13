c35c714430042e00c8183f7edc1a76843336de16
Connor Henke <elcondordelanoche@gmail.com>
2019-12-12 20:16:37 -0600

    change to terracasting

---
+++ precache-manifest.945aa82248892ccf4b775beeccb85eb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b5497d02056bef3370a812e031bf83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc9ee80341769cc9611",
+    "url": "/static/css/main.6edd7e12.chunk.css"
+  },
+  {
+    "revision": "5d26f030953261561331",
+    "url": "/static/js/2.12a3e438.chunk.js"
+  },
+  {
+    "revision": "adc9ee80341769cc9611",
+    "url": "/static/js/main.817ea1ff.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "201760b0943fd7290c9b43f4781542d9",
+    "url": "/static/media/card_back.201760b0.png"
+  },
+  {
+    "revision": "9604125a426dd055bd0f8a270b240d12",
+    "url": "/static/media/city2 2.9604125a.png"
+  },
+  {
+    "revision": "3d9c653f1739594d81e350f743049c6b",
+    "url": "/static/media/heat.3d9c653f.png"
+  },
+  {
+    "revision": "93b77ec6b41f5a6f3ae7afdfd5fc4ab6",
+    "url": "/static/media/mars.93b77ec6.png"
+  },
+  {
+    "revision": "f1faada36e64879faad41aeeb189ab7d",
+    "url": "/static/media/oceanable.f1faada3.png"
+  }
+]);
